2c86a00ddd03283dba81d18fa548f1ea19593c93
Charlie Sharpsteen <source@sharpsteen.net>
2018-03-10 20:55:58 -0800

    Add Windows support
    This commid adds support for Windows by using the Win32 `Sleep( )` function.

--- ext/ext/usleep.c
+++ ext/ext/usleep.c
@@ -1,13 +1,22 @@
 #include <ruby.h>
 
+#ifdef HAVE_RUBY_WIN32_H
+#include <windows.h>
+#else
 #include <unistd.h>
+#endif
 
 static VALUE
 do_sleep(VALUE mod, VALUE time_to_sleep)
 {
   unsigned int naptime = NUM2UINT(time_to_sleep);
 
+#ifdef HAVE_RUBY_WIN32_H
+  // Win32 sleeps by millisecond.
+  Sleep(naptime * 1000);
+#else
   sleep(naptime);
+#endif
 
   return Qnil;
 }
